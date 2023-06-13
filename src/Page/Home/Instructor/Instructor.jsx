1d1d0b054d2335b0aef5890ef1d8ac3c58180372
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-06-13 17:58:36 +0600

    add instructor card

--- src/Page/Home/Instructor/Instructor.jsx
+++ src/Page/Home/Instructor/Instructor.jsx
@@ -14,18 +14,20 @@ const Instructor = () => {
   return (
     <div className="grid grid-cols-2 gap-5 md:mx-10 py-20">
       {instructors.map((instructor) => (
-        <div key={instructor._id} className="card lg:card-side bg-base-100 shadow-xl">
-          <figure>
+        <div key={instructor._id} className="card lg:card-side bg-base-100 p-5 shadow-xl">
+          <figure >
             <img
+              className="w-40 h-40 rounded-full "
               src={instructor.image}
               alt="Album"
             />
           </figure>
-          <div className="card-body">
-            <h2 className="card-title">{instructor.name}</h2>
-            <p>{instructor.email}</p>
+          <div className="card-body items-center  ">
+            <h2 className="card-title">Name : {instructor.name}</h2>
+            <h2 className="card-title">Name : {instructor.email}</h2>
+            <p>Roll  {instructor.email}</p>
             <div className="card-actions justify-end">
-              <button className="btn btn-primary">Listen</button>
+              <button className="btn btn-primary">see classes</button>
             </div>
           </div>
         </div>
