cd2b60c0ae62b0fcc53bb5a657984e4f81d85a8e
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-06-14 17:23:00 +0600

    add class page some style

--- src/Page/Home/Classes/Classes.jsx
+++ src/Page/Home/Classes/Classes.jsx
@@ -1,6 +1,5 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
-import { AuthContext } from "../../../Provider/AuthProvider";
 import Swal from "sweetalert2";
 import useUsers from "../../../Hooks/useUsers";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -9,57 +8,77 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/autoplay";
-import { useEffect, useState } from "react";
 
 import { Autoplay, Navigation, Pagination } from "swiper";
+import { AuthContext } from "../../../Provider/AuthProvider";
 
 const Classes = () => {
-  const status = "approved";
-  const { user } = useContext(AuthContext);
   const [allUser] = useUsers();
   const location = useLocation();
+  const { user } = useContext(AuthContext);
   const navigator = useNavigate();
+  const [userRoll, setUserRoll] = useState('student');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const findUser = allUser?.find((rolls) => rolls?.email === user?.email);
+    const userStatus = findUser?.role;
+    setUserRoll(userStatus);
+  }, [allUser, user]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const fetchedUserRoll = 'admin';
+      setUserRoll(fetchedUserRoll);
+      setLoading(false);
+    }, 2000);
+  }, []);
 
   const { data } = useQuery({
     queryKey: ["data"],
     queryFn: () =>
-      fetch(`https://serene-soul-yoga-server-mdarefineahamedjoy.vercel.app/approveClass/?status=approved`).then((res) =>
-        res.json()
-      ),
+      fetch(`https://serene-soul-yoga-server-mdarefineahamedjoy.vercel.app/approveClass/?status=approved`)
+        .then((res) => res.json()),
   });
 
-  const handelSelectedClass = (classes) => {
+  const handleSelectedClass = (classes) => {
     if (!user?.email) {
       Swal.fire({
-        position: "center",
-        icon: "success",
-        title: "Place Login Fist ",
+        position: 'center',
+        icon: 'success',
+        title: 'Please Login First',
         showConfirmButton: false,
         timer: 1500,
       });
-      return navigator("/login");
+      return navigator('/login');
     }
+
     const classId = classes._id;
     delete classes._id;
     const userEmail = user?.email;
+
     fetch(`https://serene-soul-yoga-server-mdarefineahamedjoy.vercel.app/allClasses/select`, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "content-type": "application/json",
+        'content-type': 'application/json',
       },
       body: JSON.stringify({ ...classes, classId, userEmail }),
     })
       .then((res) => res.json())
       .then((data) => {
         Swal.fire({
-          position: "center",
-          icon: "success",
-          title: "Confirm Select This Class ",
+          position: 'center',
+          icon: 'success',
+          title: 'Confirm Select This Class',
           showConfirmButton: false,
           timer: 1500,
         });
       });
   };
+
+  if (loading) {
+    return <span className="loading loading-bars loading-lg"></span>;
+  }
 
   return (
     <div>
@@ -75,16 +94,14 @@ const Classes = () => {
           pagination={{
             clickable: true,
           }}
-     
           modules={[Autoplay, Pagination, Navigation]}
         >
           {data?.map((slider) => (
             <SwiperSlide key={slider?._id}>
               <div className="relative">
-              <img  className="h-[400px]" src={slider?.photoUrl} alt="" />
-              <h3 className="font-semibold  absolute bottom-0  left-20 text-white text-2xl">{slider.className}</h3>
+                <img className="h-[400px]" src={slider?.photoUrl} alt="" />
+                <h3 className="font-semibold absolute bottom-0 left-20 text-white text-2xl">{slider.className}</h3>
               </div>
-
             </SwiperSlide>
           ))}
         </Swiper>
@@ -93,35 +110,28 @@ const Classes = () => {
       <PageTitle
         title="Building a Strong Foundation in Yoga Fundamentals"
         subTitle="Cultivating Harmony, Strength, and Flexibility through Mindful Movement, Breath Control, and Meditation Techniques for Overall Well-being and Inner Balance"
-      ></PageTitle>
+      />
+
       <div className="grid grid-cols-3 px-4 gap-5">
         {data?.map((classes) => (
           <div
-            key={classes._id}
-            className={`card  bg-base-100 shadow-xl ${
-              parseInt(classes.availableSite) === 0
-                ? "bg-red-500"
-                : "bg-slate-400"
+            key={classes?._id}
+            className={`card bg-base-100 shadow-xl ${
+              parseInt(classes.availableSite) === 0 ? 'bg-red-500' : ''
             }`}
           >
-            <figure className="">
-              <img
-                className=" w-full h-64"
-                src={classes.photoUrl}
-                alt="Movie"
-              />
+            <figure>
+              <img className="w-full h-64" src={classes?.photoUrl} alt="Movie" />
             </figure>
-            <div className="card-body  ">
-              <h2 className="card-title">{classes.className}</h2>
-              <p>{classes.name}</p>
-              <p>Site {classes.availableSite}</p>
-              <p>Price {classes.price}</p>
-              <div className="card-actions justify-end">
+            <div className="card-body">
+              <h2 className="card-title">Class Name : {classes?.className}</h2>
+              <p>Instructor Name :  {classes?.name}</p>
+              <p>Available Site :  Site {classes?.availableSite}</p>
+              <p>Price :  ${classes?.price}</p>
+              <div className="card-actions justify-center">
                 <button
-                  disabled={
-                    user?.role === "instructor" || user?.role === "admin"
-                  }
-                  onClick={() => handelSelectedClass(classes)}
+                  disabled={userRoll === 'instructor' || userRoll === 'admin'}
+                  onClick={() => handleSelectedClass(classes)}
                   className="btn"
                 >
                   selected
