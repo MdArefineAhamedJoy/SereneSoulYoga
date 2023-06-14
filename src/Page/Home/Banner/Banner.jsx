00187a69f9ea57e440703b26f96942bccbcadbcb
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-06-14 12:42:24 +0600

    add animation

--- src/Page/Home/Banner/Banner.jsx
+++ src/Page/Home/Banner/Banner.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/autoplay";
 import { Autoplay, Navigation, Pagination } from "swiper";
+
 const Banner = () => {
   const [creosol, setCresol] = useState([]);
 
@@ -14,6 +16,12 @@ const Banner = () => {
         setCresol(data);
       });
   }, []);
+
+  const slideAnimation = {
+    initial: { opacity: 0, y: 20 },
+    animate: { opacity: 1, y: 0 },
+    transition: { duration: 0.5 }
+  };
 
   return (
     <>
@@ -28,17 +36,42 @@ const Banner = () => {
         pagination={{
           clickable: true,
         }}
-        // navigation={true}
         modules={[Autoplay, Pagination, Navigation]}
       >
         {creosol.map((data) => (
           <SwiperSlide key={data._id}>
             <div className="relative">
-              <img className="w-full h-screen" src={data.image} alt="" />
-              <div className="absolute top-20 left-10 right-20">
-                <h2>{data.title}</h2>
-                <p>{data.description}</p>
-              </div>
+              <motion.img
+                className="w-full h-screen opacity-80" // Add the opacity class here
+                src={data.image}
+                alt=""
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.2 }}
+              />
+              <motion.div
+                className="absolute top-20 left-10 right-20"
+                initial={slideAnimation.initial}
+                animate={slideAnimation.animate}
+                transition={slideAnimation.transition}
+              >
+                <motion.h2
+                  className="text-4xl text-white italic font-serif"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 0.2 }}
+                >
+                  {data.title}
+                </motion.h2>
+                <motion.p
+                  className="text-xl pt-5 w-1/2 text-[#7e8446] italic font-sans"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 0.4 }}
+                >
+                  {data.description}
+                </motion.p>
+              </motion.div>
             </div>
           </SwiperSlide>
         ))}
@@ -48,3 +81,4 @@ const Banner = () => {
 };
 
 export default Banner;
+
--- src/Page/Home/PopularInstructor/PopularInstructor.jsx
+++ src/Page/Home/PopularInstructor/PopularInstructor.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
+import { motion } from "framer-motion";
 import PageTitle from "../../../Components/PageTitle";
+
 const PopularInstructor = () => {
   const { isLoading, error, data, refetch } = useQuery({
     queryKey: ["data"],
@@ -16,21 +18,27 @@ const PopularInstructor = () => {
         title="Popular Instructor"
         subTitle="Popular instructor inspires students with engaging teaching methods, fostering a love for learning and nurturing their talents."
       ></PageTitle>
-      <div  className="grid md:grid-cols-3 md:gap-8 gap-3 w-full grid-cols-1 md:w-11/12 mx-auto   ">
+      <div className="grid md:grid-cols-3 md:gap-8 gap-3 w-full grid-cols-1 md:w-11/12 mx-auto">
         {data?.map((instructor) => (
-          <div key={instructor._id} className="card  bg-base-100 shadow-xl duration-500   cursor-pointer">
+          <motion.div
+            key={instructor._id}
+            className="card bg-base-100 shadow-xl duration-500 cursor-pointer"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
             <figure className="px-10 pt-8">
-              <img
+              <motion.img
                 src={instructor.image}
                 alt="instructor image"
-                className="w-40 h-40 rounded-full transition duration-300 ease-in-out transform  hover:scale-105"
+                className="w-40 h-40 rounded-full transition duration-300 ease-in-out transform"
+                whileHover={{ scale: 1.1 }}
               />
             </figure>
             <div className="card-body items-center text-center">
               <h2 className="card-title">{instructor.name}</h2>
               <h2 className="card-title">{instructor.email}</h2>
             </div>
-          </div>
+          </motion.div>
         ))}
       </div>
     </div>
