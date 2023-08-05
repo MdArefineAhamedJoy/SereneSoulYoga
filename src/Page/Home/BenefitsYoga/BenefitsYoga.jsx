40a3a22ea462bb0250f420262769152f5ad4d4cd
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-08-06 01:11:24 +0600

    add yoga advantage section and package

--- src/Page/Home/BenefitsYoga/BenefitsYoga.jsx
+++ src/Page/Home/BenefitsYoga/BenefitsYoga.jsx
@@ -35,8 +35,8 @@ const BenefitsYoga = () => {
         <div className="mx-auto">
             <p className="font-bold text-3xl mb-5">Benefited of Yoga </p>
           <ul >
-            {benefits.map((benefit) => (
-              <li className="my-1 text-lg font-semibold flex items-center gap-3"><BsCircle className="h-3 w-3 text-blue-600"></BsCircle>  {benefit}</li>
+            {benefits.map((benefit , index) => (
+              <li key={index} className="my-1 text-lg font-semibold flex items-center gap-3"><BsCircle className="h-3 w-3 text-blue-700"></BsCircle>  {benefit}</li>
             ))}
           </ul>
         </div>
--- src/Page/Home/MemberShip/MemberShip.jsx
+++ src/Page/Home/MemberShip/MemberShip.jsx
@@ -3,6 +3,7 @@ import PageTitle from "../../../Components/PageTitle";
 
 const MemberShip = () => {
   const [packages, setPackages] = useState([]);
+  const [colors , setColors] = useState("")
   useEffect(() => {
     fetch(`http://localhost:5000/memberShip/package`)
       .then((res) => res.json())
@@ -15,44 +16,57 @@ const MemberShip = () => {
     <div className="md:mx-10">
       <div></div>
       <PageTitle
-        title="Choose a membership to fit your needs"
-        subTitle=""
+        title="READY TO GET STARTED?"
+        subTitle="Join today and get a full year of Yoga International membership for $169.00/year – a 20% savings on our monthly membership price."
         url=""
       ></PageTitle>
-      <div className="grid grid-cols-3 gap-5">
-        {packages.map(
-          ({
-            _id,
-            type,
-            access,
-            exclusiveAccess,
-            curatedPrograms,
-            livestreamDiscount,
-            favoriteTeachers,
-            curatedClasses,
-            appAccess,
-            favoriteClasses,
-            price,
-          }) => (
-            <div className="bg-white shadow-md  rounded relative " key={_id}>
-              <div className="p-5">
-                <h3 className="mt-5 text-2xl font-semibold ">{type}</h3>
-                <p className="my-3">{access}</p>
-                <p>{exclusiveAccess}</p>
-                <p className="my-3">{curatedPrograms}</p>
-                <p>{livestreamDiscount}</p>
-                <p className="my-3">{favoriteTeachers}</p>
-                <p>{curatedClasses}</p>
-                <p className="my-3">{appAccess}</p>
-                <p>{favoriteClasses}</p>
-                <p className="mt-3 pb-6">{price}</p>
-              </div>
-              <button className=" w-full p-2 text-white uppercase font-semibold bg-blue-600 absolute bottom-0">
-                Buy Now{" "}
-              </button>
+      <div className=" relative">
+        <table className="table p-10">
+          {/* head */}
+          <thead>
+            <tr className=" bg-base-200 border-b border-black">
+              <td className="font-normal text-lg">Plus applicable tax</td>
+              <th className= {`text-center text-black  ${colors === "monthly" ? "bg-gray-200" : "bg-base-200" }`}>
+                <span className="font-extrabold text-xl">$19.99 </span> <br />{" "}
+                billed monthly{" "}
+              </th>
+              <th className={`text-center text-black  ${colors === "annually" ? "bg-gray-200" : "bg-base-200" }`}>
+                <span className="font-extrabold text-xl">$169.00</span> <br />{" "}
+                billed annually
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {packages.map(({ access, monthly, annual , _id}) => (
+              <tr className="bg-base-200 border-b border-black" key={_id}>
+                <td className="w-8/12">{access}</td>
+                <td className={`text-center ${colors === "monthly" ? "bg-gray-200" : "bg-base-200" }`}>
+                  {monthly === "true"
+                    ? "loading"
+                    : monthly === "discount"
+                    ? "15%"
+                    : ""}
+                </td>
+                <td className={`text-center ${colors === "annually" ? "bg-gray-200" : "bg-base-200" }`}>
+                  {annual === "true"
+                    ? "loading"
+                    : annual === "discount"
+                    ? "20%"
+                    : ""}
+                </td>
+              </tr>
+            ))}
+            <div className={`bg-base-200 px-16 shadow-md  rounded-md py-5 text-center absolute top-[-50px] right-[207px] font-semibold ${colors === "monthly" ? "bg-green-600 text-white" : "bg-base-200 text-black" } `} onClick={()=>setColors("monthly")}>
+              Monthly
             </div>
-          )
-        )}
+            <div className={`bg-base-200 px-16 shadow-md rounded-md py-5 text-center absolute top-[-50px] right-[10px] ${colors === "annually"? "bg-green-600 text-white": "bg-base-200 text-black " }`} onClick={()=>setColors("annually")}>
+            <div className="relative" >
+            <p className="absolute px-11 py-1 rounded-md top-[-47px] right-[-48px] bg-yellow-400"> saved20% </p>
+            </div>
+             <p> Annually</p>
+            </div>
+          </tbody>
+        </table>
       </div>
     </div>
   );
