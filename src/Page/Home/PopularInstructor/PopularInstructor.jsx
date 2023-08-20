46dc1f9c984bed0919097a0174dc27a939dfa6d8
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-08-21 02:34:38 +0600

    add popular instructor style change

--- src/Page/Home/PopularInstructor/PopularInstructor.jsx
+++ src/Page/Home/PopularInstructor/PopularInstructor.jsx
@@ -7,7 +7,6 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/autoplay";
 import { Autoplay, Navigation, Pagination } from "swiper";
-import Instructor from "./../Instructor/Instructor";
 
 const PopularInstructor = () => {
   const { isLoading, error, data, refetch } = useQuery({
@@ -23,25 +22,29 @@ const PopularInstructor = () => {
         title="Popular Instructor"
         subTitle="Popular instructor inspires students with engaging teaching methods, fostering a love for learning and nurturing their talents."
       ></PageTitle>
-      <div>
+      <div className="w-11/12 mx-auto">
         <Swiper
           slidesPerView={4}
           autoplay={{
-            delay: 2,
+            delay: 1,
             disableOnInteraction: false,
           }}
           speed={3000}
           loop={true}
+          navigation={true}
           pagination={{
             clickable: true,
           }}
           modules={[Autoplay, Pagination, Navigation]}
         >
           {data?.map((Instructor) => (
-            <div key={Instructor?._id} className="grid md:grid-cols-4 gap-3 rounded-none  ">
-              <SwiperSlide >
+            <div
+              key={Instructor?._id}
+              className="grid md:grid-cols-3 gap-3 rounded-none  group"
+            >
+              <SwiperSlide>
                 <motion.div
-                  className=" shadow-xl text-white duration-500 cursor-pointer h-96 bg-[#227179] hover:bg-[#227179] mx-1 rounded-md"
+                  className=" shadow-xl text- duration-500 cursor-pointer h-full bg-white hover:text-[#2b666c] text-neutral-600  mx-1 rounded-md"
                   whileHover={{ scale: 1.01 }}
                   whileTap={{ scale: 0.95 }}
                 >
@@ -49,13 +52,17 @@ const PopularInstructor = () => {
                     <motion.img
                       src={Instructor?.image}
                       alt="instructor image"
-                      className="w-11/12 h-60 rounded-t-md mx-auto  transition duration-700  transform"
-                      whileHover={{ scale: 1.1 }}
+                      className="w-full h-44 rounded-t-md mx-auto  transition duration-700  transform"
                     />
                   </figure>
-                  <div className="card-body items-center text-center">
-                    <h2 className="">{Instructor?.name}</h2>
+                  <div className="mt-5 items-center text-center">
+                    <h2 className="font-bold uppercase text-lg">
+                      {Instructor?.name}
+                    </h2>
                     <h2 className="">{Instructor?.email}</h2>
+                    <button className="bg-[#227179] mb-5  px-7 font-semibold hover:bg-[#153f44] duration-300  py-2 my-2 uppercase text-sm text-white rounded-ss-xl rounded-ee-xl">
+                      details 
+                    </button>
                   </div>
                 </motion.div>
               </SwiperSlide>
