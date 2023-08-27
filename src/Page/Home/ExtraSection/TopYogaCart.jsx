b35c2672fdba90ed1ec5583e3feff1afc7d43ab3
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-08-27 22:33:27 +0600

    add up to data

--- src/Page/Home/ExtraSection/TopYogaCart.jsx
+++ src/Page/Home/ExtraSection/TopYogaCart.jsx
@@ -2,14 +2,20 @@ import React, { useEffect, useState } from "react";
 
 const TopYogaCart = ({ yoga }) => {
   const { title, image, description , textColor , border } = yoga;
-  console.log(textColor , border )
+  const cardStyle = {
+    borderColor: border,
+  };
+
+  const textStyle = {
+    color: textColor,
+  };
   return (
     <div className="card card-compact text-center group">
       <div className="card-body">
-        <h2   className={`text-3xl font-semibold mb-8  ${textColor}` }>{title}</h2>
+        <h2   className={`text-3xl font-semibold mb-8 ` } style={textStyle}>{title}</h2>
         <figure>
           <img
-            className={`w-36   h-36 border-4  transform group-hover:scale-125 transition-transform duration-300 rounded-full ${border}`}
+            className={`w-36   h-36 border-4  transform group-hover:scale-125 transition-transform duration-300 rounded-full `} style={cardStyle} 
             src={image}
             alt="Shoes"
           />
